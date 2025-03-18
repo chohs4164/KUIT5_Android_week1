0762e07ba09f00cc0b61662c8d7fde52fc5e5bec
chohs4164 <chohs4164@naver.com>
2025-03-18 13:17:09 +0900

    week1

--- app/src/main/java/com/example/kuit_1th_weeks/MainActivity.kt
+++ app/src/main/java/com/example/kuit_1th_weeks/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.kuit_1th_weeks
 
+
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kuit_1th_weeks.ui.theme.KUIT_1th_weeksTheme
 
 class MainActivity : ComponentActivity() {
@@ -68,5 +87,166 @@ fun ProfileCard(modifier: Modifier = Modifier) {
 fun ProfileCardPreview() {
     KUIT_1th_weeksTheme {
         ProfileCard()
+    }
+}
+
+@Composable
+fun Sample(){
+    Column{
+        Box(
+            modifier = Modifier
+                .padding (
+                    top = 27.dp,
+                    start = 29.dp
+                )
+                .width(60.dp)
+                .height(60.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .background(color = Color.Black)
+        )
+        Box(
+            modifier = Modifier
+                .padding(top=20.dp,start=29.dp)
+                .size(60.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .border(
+                    width=2.dp,
+                    color= Color.Black,
+                    shape = RoundedCornerShape(20.dp)
+                )
+        )
+        Box(
+            modifier = Modifier
+                .padding(top=20.dp,start=20.dp)
+                .size(76.dp)
+                .border(
+                    width=1.dp,
+                    color= Color.Black
+                )
+                .padding(
+                    top=8.dp,
+                    bottom=8.dp,
+                    start=8.dp,
+                    end=8.dp
+                )
+                .background(color= Color.Red)
+
+        )
+        Image(
+            painter = painterResource(R.drawable.image_dog),
+            contentDescription = "dog image",
+            modifier=Modifier
+                .padding(top=20.dp, start = 29.dp)
+                .size(80.dp)
+        )
+    }
+}
+
+@Composable
+fun CircleSample(){
+    Column{
+        Box(
+            modifier = Modifier
+                .padding (
+                    top = 27.dp,
+                    start = 29.dp
+                )
+                .width(60.dp)
+                .height(60.dp)
+                .clip(CircleShape)
+                .background(color = Color.Black)
+        )
+        Box(
+            modifier = Modifier
+                .padding(top=20.dp,start=29.dp)
+                .size(60.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .border(
+                    width=3.dp,
+                    color= Color.Black,
+                    shape = CircleShape
+                )
+        )
+        Box(
+            modifier = Modifier
+                .padding(top=20.dp,start=20.dp)
+                .size(76.dp)
+                .border(
+                    width=1.dp,
+                    color= Color.Black
+                )
+                .padding(
+                    top=8.dp,
+                    bottom=8.dp,
+                    start=8.dp,
+                    end=8.dp
+                )
+                .padding(8.dp)
+                .clip(CircleShape)
+                .background(color= Color.Blue)
+        )
+        Image(
+            painter = painterResource(R.drawable.image_dog),
+            contentDescription = "dog image",
+            modifier = Modifier
+                .padding(top = 20.dp, start = 29.dp)
+                .size(80.dp)
+                .clip(CircleShape)
+        )
+        Box(
+            modifier = Modifier
+                .padding(top=20.dp,start=20.dp)
+                .size(140.dp)
+                .border(
+                    width=1.dp,
+                    color= Color.Black,
+                    shape = RoundedCornerShape(10.dp)
+                )
+        ) {
+            Image(
+                painter = painterResource(R.drawable.baebae),
+                contentDescription = "baebae",
+                modifier = Modifier
+                    .padding(top = 10.dp, start = 10.dp)
+                    .size(40.dp)
+                    .clip(RoundedCornerShape(10.dp))
+
+            )
+            Text(
+                text = "이름 : 조현승",
+                fontSize = 8.sp,
+                modifier = Modifier
+                    .padding(top = 60.dp,start=10.dp)
+            )
+            Text(
+                text = "학번 : 202315323",
+                fontSize = 8.sp,
+                modifier = Modifier
+                    .padding(top = 75.dp,start=10.dp)
+            )
+            Text(
+                text = "학과 : 컴퓨터공학부",
+                fontSize = 8.sp,
+                modifier = Modifier
+                    .padding(top = 90.dp,start=10.dp)
+            )
+            Text(
+                text = "생년월일 : 2001/11/27",
+                fontSize = 8.sp,
+                modifier = Modifier
+                    .padding(top = 105.dp,start=10.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SamplePreview(){
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        //Sample()
+        CircleSample()
     }
 }
